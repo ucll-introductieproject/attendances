c778fe66803ffa37488113d41cdfb4c03bdd3b9f
Frederic Vogels <frederic.vogels@ucll.be>
2022-07-05 21:55:50 +0200

    absentees package

---
+++ absentees/countdown.py
@@ -0,0 +1,14 @@
+class Countdown:
+    def __init__(self, duration):
+        self.__duration = duration
+        self.__time_left = duration
+
+    def tick(self, elapsed_seconds):
+        self.__time_left = max(0, self.__time_left - elapsed_seconds)
+
+    @property
+    def ready(self):
+        return self.__time_left == 0
+
+    def reset(self):
+        self.__time_left = self.__duration
