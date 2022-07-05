a60f7d338babcee91c412636d0c018f38513e36d
Frederic Vogels <frederic.vogels@ucll.be>
2022-07-05 17:03:22 +0200

    --ignore

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import cv2
 from pyzbar.pyzbar import decode
 from contextlib import contextmanager
-from time import sleep
+from time import sleep, monotonic
 import click
 import chime
+
+
+def now():
+    return monotonic()
 
 
 @contextmanager
@@ -22,23 +26,38 @@ def video_capture(source):
 @click.option('--theme', help=f"One of {', '.join(chime.themes())}", default='big-sur')
 @click.option('--source', help='Index of video source', default=0, type=int)
 @click.option('--quiet', is_flag=True, help='No sound')
-@click.option('--delay', help='Milliseconds between polls', default=500, type=int)
-def main(theme, source, quiet, delay):
+@click.option('--wait', help='Milliseconds between polls', default=500, type=int)
+@click.option('--ignore', help='Milliseconds during which to ignore repeat scans', default=1000, type=int)
+def main(theme, source, quiet, wait, ignore):
+    def initialize():
+        print('Initializing...')
+        chime.theme(theme)
+
     def play_success_sound():
         if not quiet:
             chime.success()
 
-    print('Initializing...')
-    chime.theme(theme)
-    with video_capture(source) as read_frame:
-        print('Ready to scan QR codes!')
-        while True:
-            decoded = decode(read_frame())
-            if decoded:
-                print(decoded)
-                play_success_sound()
-            sleep(delay / 1000)
+    def scan():
+        scanned = {}
 
+        with video_capture(source) as read_frame:
+            print('Ready to scan QR codes!')
+            while True:
+                for decoded in decode(read_frame()):
+                    data = decoded.data.decode('utf-8')
+                    if data not in scanned:
+                        scanned[data] = now()
+                        print(data)
+                        play_success_sound()
+                    else:
+                        delta = now() - scanned[data]
+                        if delta > ignore / 1000:
+                            print(f'Already scanned {data}')
+
+                sleep(wait / 1000)
+
+    initialize()
+    scan()
 
 
 if __name__ == '__main__':
