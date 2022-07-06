0f53bdf10be8316faed29196a85e20cf5f608605
Frederic Vogels <frederic.vogels@ucll.be>
2022-07-06 09:52:21 +0200

    rearranged code

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import cv2
-from contextlib import contextmanager
 import click
 import absentees.settings
 from pathlib import Path
@@ -24,16 +22,22 @@ def tui(quiet):
    import absentees.tui as tui
    tui.run(load_settings(), quiet)
 
+cli.add_command(tui)
+
 
 @click.command()
 def gui():
     import absentees.gui as gui
     gui.run(load_settings())
 
+cli.add_command(gui)
+
 
 @click.group()
 def config():
     pass
+
+cli.add_command(config)
 
 
 @click.command('show')
@@ -41,10 +45,14 @@ def config_show():
     settings = load_settings()
     print(json.dumps(settings.data, indent=4, sort_keys=True))
 
+config.add_command(config_show)
+
 
 @click.command('delete')
 def config_delete():
     SETTINGS_PATH.unlink()
+
+config.add_command(config_delete)
 
 
 @click.command()
@@ -53,13 +61,8 @@ def cameras():
     for index, id in enumerate(Capturer.cameras()):
         print(f'[{index}] {id}')
 
-
-cli.add_command(tui)
-cli.add_command(gui)
-cli.add_command(config)
 cli.add_command(cameras)
-config.add_command(config_show)
-config.add_command(config_delete)
+
 
 if __name__ == '__main__':
     cli()
