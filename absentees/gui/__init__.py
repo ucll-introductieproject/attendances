c7d70fc84cbb82f60bc781fda50c1128b287fe03
Frederic Vogels <frederic.vogels@ucll.be>
2022-07-07 18:53:56 +0200

    cleanup

--- absentees/gui/__init__.py
+++ absentees/gui/__init__.py
@@ -54,77 +54,75 @@ def run(settings, sound_player):
     pygame.init()
 
     channel = Channel()
+    capture_fps = settings['capture.rate']
+    clock = Clock(settings['frame-rate'])
+    font = pygame.font.SysFont(None, settings['font-size'])
+    window_size = get_window_size(settings)
+    capture_width, capture_height = settings['capture.width'], settings['capture.height']
+    current_screen = None
 
-    with server(channel):
-        capture_fps = settings['capture.rate']
-        clock = Clock(settings['frame-rate'])
-        font = pygame.font.SysFont(None, settings['font-size'])
+    logging.debug(f'Creating window with size {window_size[0]}x{window_size[1]}')
+    render_surface = pygame.display.set_mode(window_size)
 
-        window_size = get_window_size(settings)
-        capture_width, capture_height = settings['capture.width'], settings['capture.height']
-        current_screen = None
+    capture_surface_cell = Cell(pygame.Surface((capture_width, capture_height)))
+    capture_ndarray_cell = capture_surface_cell.derive(lambda x: pygame.surfarray.array3d(x).swapaxes(0, 1))
+    capture_grayscale = capture_ndarray_cell.derive(lambda x: cv2.cvtColor(x, cv2.COLOR_BGR2GRAY))
 
-        logging.debug(f'Creating window with size {window_size[0]}x{window_size[1]}')
-        render_surface = pygame.display.set_mode(window_size)
+    countdown = Countdown(1 / capture_fps)
 
-        capture_surface_cell = Cell(pygame.Surface((capture_width, capture_height)))
-        capture_ndarray_cell = capture_surface_cell.derive(lambda x: pygame.surfarray.array3d(x).swapaxes(0, 1))
-        capture_grayscale = capture_ndarray_cell.derive(lambda x: cv2.cvtColor(x, cv2.COLOR_BGR2GRAY))
+    with server(channel), auto_capture(capture_surface_cell, 1 / 30) as auto_capturer:
+        screen_data = {
+            'switch_screen': switch_screen,
+            'face_detector': FaceDetector(),
+            'qr_scanner': QRScanner(),
+            'capture_surface_cell': capture_surface_cell,
+            'capture_ndarray_cell': capture_ndarray_cell,
+            'sound_player': sound_player,
+            'qr_highlight_color': settings.color('qr.highlight-color'),
+            'font': font,
+            'capture_rate': settings['qr.capture-rate'],
+            'freeze_time': settings['qr.freeze-time'],
+        }
 
-        countdown = Countdown(1 / capture_fps)
-        with auto_capture(capture_surface_cell, 1 / 30) as auto_capturer:
-            screen_data = {
-                'switch_screen': switch_screen,
-                'face_detector': FaceDetector(),
-                'qr_scanner': QRScanner(),
-                'capture_surface_cell': capture_surface_cell,
-                'capture_ndarray_cell': capture_ndarray_cell,
-                'sound_player': sound_player,
-                'qr_highlight_color': settings.color('qr.highlight-color'),
-                'font': font,
-                'capture_rate': settings['qr.capture-rate'],
-                'freeze_time': settings['qr.freeze-time'],
-            }
+        current_screen = IdleScreen(screen_data, capture_surface_cell)
 
-            current_screen = IdleScreen(screen_data, capture_surface_cell)
+        clock.add_observer(countdown.tick)
+        clock.add_observer(auto_capturer.tick)
+        clock.add_observer(lambda dt: current_screen.tick(dt))
 
-            clock.add_observer(countdown.tick)
-            clock.add_observer(auto_capturer.tick)
-            clock.add_observer(lambda dt: current_screen.tick(dt))
+        active = True
+        while active:
+            if channel.message_from_server_waiting:
+                logging.debug('Client receives message from server')
+                request = channel.receive_from_server()
+                print(request)
+                logging.debug('Client answers server')
+                channel.respond_to_server('ok'.encode('utf-8'))
 
-            active = True
-            while active:
-                if channel.message_from_server_waiting:
-                    logging.debug('Client receives message from server')
-                    request = channel.receive_from_server()
-                    print(request)
-                    logging.debug('Client answers server')
-                    channel.respond_to_server('ok'.encode('utf-8'))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    active = False
 
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        active = False
+            clock.update()
 
-                clock.update()
+            # if countdown.ready:
+            #     countdown.reset()
+            #     capture()
 
-                # if countdown.ready:
-                #     countdown.reset()
-                #     capture()
+            #     if decoded := qr_scanner.scan(capture_ndarray.value):
+            #         data = decoded[0]
+            #         pygame.draw.polygon(capture_surface.value, highlight_color, data.polygon, width=2)
 
-                #     if decoded := qr_scanner.scan(capture_ndarray.value):
-                #         data = decoded[0]
-                #         pygame.draw.polygon(capture_surface.value, highlight_color, data.polygon, width=2)
+            #         faces = face_detector.detect(capture_grayscale.value)
+            #         for (x, y, w, h) in faces:
+            #             rect = pygame.Rect(x, y, w, h)
+            #             pygame.draw.rect(capture_surface.value, highlight_color, rect, width=2)
 
-                #         faces = face_detector.detect(capture_grayscale.value)
-                #         for (x, y, w, h) in faces:
-                #             rect = pygame.Rect(x, y, w, h)
-                #             pygame.draw.rect(capture_surface.value, highlight_color, rect, width=2)
+            #         print(data.data)
+            #         sound_player.success()
 
-                #         print(data.data)
-                #         sound_player.success()
+            #     if show_video:
+            #         render_surface.blit(capture_surface.value, (0, 0))
 
-                #     if show_video:
-                #         render_surface.blit(capture_surface.value, (0, 0))
-
-                current_screen.render(render_surface)
-                pygame.display.flip()
+            current_screen.render(render_surface)
+            pygame.display.flip()
