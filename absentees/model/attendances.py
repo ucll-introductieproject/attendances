efeb0f817e7cb306ef3dadb7c011df8d968abc8d
Frederic Vogels <frederic.vogels@ucll.be>
2022-07-14 14:02:43 +0200

    attendances written to file

--- absentees/model/attendances.py
+++ absentees/model/attendances.py
@@ -1,9 +1,15 @@
 from absentees.model.person import Person
+import json
 
 
 class Attendances:
     def __init__(self, names):
-        self.__people = {name: Person(name) for name in names}
+        self.__people = {}
+        for name in names:
+            person = Person(name)
+            self.__people[name] = person
+            person.present.add_observer(self.__on_person_changed)
+        self.__write_to_file()
 
     def __getitem__(self, name):
         return self.__people[name]
@@ -18,3 +24,11 @@ class Attendances:
 
     def register(self, name):
         self.__people[name].present.value = True
+
+    def __on_person_changed(self):
+        self.__write_to_file()
+
+    def __write_to_file(self):
+        with open('attendances.txt', 'w') as file:
+            data = {person.name: person.present.value for person in self.people}
+            json.dump(data, file)
