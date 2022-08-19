36e439731d6f09856559ea6b64027e33f323bae2
Frederic Vogels <frederic.vogels@ucll.be>
2022-08-19 18:44:07 +0200

    capturers -> capturing

--- src/app.py
+++ src/app.py
@@ -90,7 +90,7 @@ config.add_command(config_delete)
 
 @click.command()
 def cameras():
-    from attendances.tools.capturers import Capturer
+    from attendances.tools.capturing import Capturer
     for index, id in enumerate(Capturer.cameras()):
         print(f'[{index}] {id}')
 
