b471a77c55d98100e2299c298ca3dafb144c08b3
Frederic Vogels <frederic.vogels@ucll.be>
2022-08-19 23:35:15 +0200

    cleaned up tui

--- src/attendances/tui.py
+++ src/attendances/tui.py
@@ -5,17 +5,11 @@ from attendances.model.attendances import Attendances
 from attendances.tools.analyzing import FrameAnalyzer
 from attendances.tools.capturing import DummyCapturer, VideoCapturer
 from attendances.gui import _create_frame_analyzer
-from attendances.gui.clock import Clock
-from attendances.model.model import Model
 from attendances.tools.sound import SoundPlayer
 from attendances.pipeline import *
 import time
 import json
 import attendances.commands as commands
-
-
-def now():
-    return monotonic()
 
 
 def _create_capturer(settings):
@@ -31,25 +25,6 @@ def _create_capturer(settings):
 def _create_frame_analyzer(settings):
     logging.info("Creating frame analyzer")
     return FrameAnalyzer()
-
-
-def _create_clock(settings):
-    rate = settings['frame-rate']
-    logging.info(f'Creating clock with rate {rate}')
-    return Clock(rate)
-
-
-def _create_model(settings, clock):
-    video_capturer = _create_capturer(settings.subtree('video-capturing'))
-    frame_analyzer = _create_frame_analyzer(settings.subtree('frame-analyzing'))
-    names = [str(k).rjust(5, '0') for k in range(0, 98)]
-    return Model(
-        settings=settings,
-        video_capturer=video_capturer,
-        frame_analyzer=frame_analyzer,
-        clock=clock,
-        names=names
-    )
 
 
 def _create_sound_player(settings):
