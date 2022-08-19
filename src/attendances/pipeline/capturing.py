ed530aad7555ec1b601e105574a4c1da58845189
Frederic Vogels <frederic.vogels@ucll.be>
2022-08-19 19:53:14 +0200

    capturing node

--- src/attendances/pipeline/capturing.py
+++ src/attendances/pipeline/capturing.py
@@ -1,10 +1,20 @@
-from attendances.tools.capturing import Capturer
+from attendances.tools.capturing import Capturer, VideoCapturerHandle
 
 
-class FrameSource:
-    def __init__(self, capturer):
-        assert isinstance(capturer, Capturer)
-        self.__capturer = capturer
+class FrameNode:
+    def __init__(self, capturer_handle, surface):
+        assert isinstance(capturer_handle, VideoCapturerHandle)
+        self.__capturer_handle = capturer_handle
+        self.__surface = surface
+        self.__observers = []
 
-    # def capture(self):
-    #     self.__capturer.
+    def on_captured(self, observer):
+        self.__observers.append(observer)
+
+    def capture(self):
+        self.__capturer_handle.capture(self.__surface)
+        self.__notify_observers()
+
+    def __notify_observers(self):
+        for observer in self.__observers:
+            observer(self.__surface)
