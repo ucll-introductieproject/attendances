546c06a4248ad958b8e3b33e53cb56ac6f2b811c
Frederic Vogels <frederic.vogels@ucll.be>
2022-08-19 19:53:24 +0200

    skipper node

---
+++ src/attendances/pipeline/skipper.py
@@ -0,0 +1,15 @@
+class Skipper:
+    def __init__(self, n):
+        self.__observers = []
+        self.__n = n
+        self.__count = 0
+
+    def on_event(self, observer):
+        self.__observers.append(observer)
+
+    def perform(self, *args, **kwargs):
+        self.__count += 1
+        if self.__count == self.__n:
+            self.__count = 0
+            for observer in self.__observers:
+                observer(*args, **kwargs)
