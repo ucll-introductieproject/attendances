dcb53a88241e4eb1ec4e67d40c034ea9cfd7ec72
Frederic Vogels <frederic.vogels@ucll.be>
2022-08-28 19:55:26 +0200

    settings comments

--- src/attendances/settings.py
+++ src/attendances/settings.py
@@ -3,11 +3,15 @@ def screen_size():
     info = pygame.display.Info()
     return (info.current_w, info.current_h)
 
+
+registration_file = r'G:\repos\ucll\introproject\absentees\data\attendances.txt'
+
+students_file = r'G:\repos\ucll\introproject\absentees\data\students.txt'
+
 frame_size = (640, 480)
 
+# Can also be set equal to screen_size() for fullscreen mode
 window_size = (1920, 1080)
-
-registration_file = 'attendances.txt'
 
 sound_theme = 'big-sur'
 
@@ -21,8 +25,11 @@ qr_transformations = [
     # 'bw_gaussian',
 ]
 
+# 0 = no limit
 frame_rate = 0
 
 show_fps = True
 
+# Lower = more responsive
+# Higher = less computationally expensive
 analyze_every_n_frames = 5
