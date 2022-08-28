de8ddd4156aa85ac76141231c77fa0f61c49f9c5
Frederic Vogels <frederic.vogels@ucll.be>
2022-08-28 10:48:02 +0200

    better file registration

--- src/attendances/registration.py
+++ src/attendances/registration.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-
+from datetime import datetime
 
 class FileRegistration:
     def __init__(self, path):
         assert isinstance(path, Path)
         self.__path = path
 
-    def register(self, id):
+    def register(self, person):
+        time_string = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+        string = f'{time_string} {person.name}'
         with self.__path.open('a') as file:
-            print(id, file=file)
+            print(string, file=file)
